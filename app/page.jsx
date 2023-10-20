fd9980295cb1b1ebb7471b76fbfef455756bf302
jagh06 <jhonguzmanhernandez659@gmail.com>
2023-10-19 22:24:07 -0600

    add env

--- app/page.jsx
+++ app/page.jsx
@@ -2,26 +2,28 @@ import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { authOptions } from "./api/auth/[...nextauth]/route";
 import Link from "next/link";
-
+import { SessionProvider } from "next-auth/react";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
   if (session) redirect("/dashboard");
   return (
-    <main>
-      <header>
-        <div c>
-          <div>
+    <SessionProvider basePath="/api/auth/">
+      <main>
+        <header>
+          <div c>
             <div>
-              <nav>
-                <Link href="">Recomendados</Link>
-                <Link href="">Hospedajes</Link>
-                <Link href="/login">Login</Link>
-              </nav>
+              <div>
+                <nav>
+                  <Link href="">Recomendados</Link>
+                  <Link href="">Hospedajes</Link>
+                  <Link href="/login">Login</Link>
+                </nav>
+              </div>
             </div>
           </div>
-        </div>
-      </header>
-    </main>
+        </header>
+      </main>
+    </SessionProvider>
   );
 }
