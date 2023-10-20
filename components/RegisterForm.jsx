e7e83cb6d45f851b86891d02054b6acdd6d3b7e5
jagh06 <jhonguzmanhernandez659@gmail.com>
2023-10-20 01:00:04 -0600

    add span in button register

--- components/RegisterForm.jsx
+++ components/RegisterForm.jsx
@@ -85,7 +85,7 @@ export default function RegisterForm() {
             placeholder="Password"
           />
           <button className="bg-green-600 text-white font-bold cursor-pointer px-6 py-2" id="inputAdd">
-            Register
+            <span>Register</span>
           </button>
 
           {error && (
